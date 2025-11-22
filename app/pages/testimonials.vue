95f672e3cc397854c271a98701e58996e1260d84
Michael Whittemore <michaelwhittemore@gmail.com>
2025-11-22 12:18:58 -0800

    styled the boxes

--- app/pages/testimonials.vue
+++ app/pages/testimonials.vue
@@ -2,20 +2,60 @@
     <div class="yellow-background full-height">
         <div class="page-content-container">
             <h1>Testimonials</h1>
-            <p>
-                I've thought to myself in the darkest of nights
-                Of freedom above me where all is alright
-                But I wake up so cold on a cracked wooden floor
-                And I've got to admit I'm not sure anymore
-                Yeah I live in this endless misery however
-                Yeah I'm gonna work on Maggie's farm forever
-            </p>
+            <div class="testimonial-container">
+                <div class="testimonial-box rounded yellow-pricing-box">
+                    I worked in her fields
+                    And under her trees
+                    I picked all her cotton
+                    It stung me like bees
+                    I shoveled her dirt
+                    When no one else would
+                    I gave her way more
+                    Than I ever should
+                    And I'm still here with nothing
+                    Whatsoever
+                    So I'm gonna work on Maggie's farm forever
+                </div>
+                <div class="testimonial-box rounded yellow-pricing-box">
+
+                    I ground down my knuckles for nothing at all
+                    I've worked on my knees till I couldn't crawl
+                    I've felt so worthless I can hardly recall
+                    A time before she had me trapped in her wall
+                    The odds of me leaving are slim to almost never
+                    Yeah I'm gonna work on Maggie's farm forever
+                </div>
+                <div class="testimonial-box rounded yellow-pricing-box">
+
+                    I've thought to myself in the darkest of nights
+                    Of freedom above me where all is alright
+                    But I wake up so cold on a cracked wooden floor
+                    And I've got to admit I'm not sure anymore
+                    Yeah I live in this endless misery however
+                    Yeah I'm gonna work on Maggie's farm forever
+                </div>
+            </div>
         </div>
     </div>
 
 </template>
 
-<script setup>
-</script>
+<style scoped>
+.testimonial-container {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    margin-top: 2rem;
+}
 
-<style scoped></style>
+.testimonial-box {
+    flex: 1;
+    border: 1px solid black;
+    background-color: #c1bad9;
+    padding: 1.5rem;
+}
+
+.testimonial-box.rounded {
+    border-radius: 12px;
+}
+</style>
